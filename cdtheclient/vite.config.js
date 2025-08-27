c4e020aa0533509308482227de5586c91c4ff2ec
jemila2 <abubakarjemiletu08@gmail.com>
2025-08-27 18:18:58 +0100

    Update vite.config.js

--- cdtheclient/vite.config.js
+++ cdtheclient/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/thesl/',  // Added slashes - this is critical for GitHub Pages
+  base: '/thesl/', 
   plugins: [
     react(),
     tailwindcss(),
