457371f73b93acb619dc1dad1893c63db0966582
jemila2 <abubakarjemiletu08@gmail.com>
2025-08-27 17:20:29 +0100

    Update vite.config.js

--- cdtheclient/vite.config.js
+++ cdtheclient/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/thesl/,
   plugins: [
     react(),
     tailwindcss(), // Tailwind v4 uses Vite plugin instead of PostCSS
